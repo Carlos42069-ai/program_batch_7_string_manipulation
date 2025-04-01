a7080c48ea71b2e5b82853402e5cc38799d0d92f
Carlos <carlosroque117@gmail.com>
2025-04-01 16:30:08 +0800

    Asks user for an input and a word to be checked in the input then added a code that checks if the word to be checked is in the input.

--- without_using_startswith_function.py
+++ without_using_startswith_function.py
@@ -1,5 +1,8 @@
 #Ask user for input. 
+ask_user = input("Enter your input: ")
+starts_with = input("Enter the word to be checked in your input: ")
 
 #Checking if the entered word starts with the inputed function parameter.
+print_results = ask_user[:len(starts_with)] == starts_with
 
 #Print if it is true or false.
