d0a6252717eb4c684409b5f5605d41efadffc721
Carlos <carlosroque117@gmail.com>
2025-04-01 14:54:32 +0800

    Fixed the name errors and made a code that prints out the outcome.

--- without_rstrip_function.py
+++ without_rstrip_function.py
@@ -1,6 +1,7 @@
 #Ask user for input with spaces after the words.
 ask_user = input("Enter a word with extra spaces after: ")
 #Remove ther extra spaces after the word.
-while user_input and user_input[-1] == " ":
-    user_input = user_input[:-1]
+while ask_user and ask_user[-1] == " ":
+    ask_user = ask_user[:-1]
 #Print the outcome.
+print("Output after removing space:", repr(ask_user))
