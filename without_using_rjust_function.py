257843d3a122240cece3ab81ca16d36e1749a060
Carlos <carlosroque117@gmail.com>
2025-04-01 16:43:10 +0800

    Added a code that adds a space depending on the total amount of characters desired and prints out the result.

--- without_using_rjust_function.py
+++ without_using_rjust_function.py
@@ -4,5 +4,6 @@ total_length = int(input("Enter the desired total length: "))
 #Calculate how many spaces to add.
 spaces_to_add = total_length - len(user_input)
 #Add spaces to the beginning.
-
+padded_string = ' ' * spaces_to_add + user_input
 #Print result.
+print("Padded string:", padded_string)
