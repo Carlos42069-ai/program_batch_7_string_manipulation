450d02a899f1f992dcc6051a1490bc4ea083c7f9
Carlos <carlosroque117@gmail.com>
2025-04-01 15:58:18 +0800

    Asks user for a word or sentence then converts the lower case characters into upper cases.

--- without_using_upper_function.py
+++ without_using_upper_function.py
@@ -1,5 +1,10 @@
 #Ask user for input.
-
+ask_user = input("Enter a word: ")
 #Convert small letters to upper case.
-
+uppercase = ""
+for char in ask_user:
+    if 'a' <= char <= 'z':
+        uppercase += chr(ord(char) - 32)
+    else:
+        uppercase_result += char
 #Print the result. 
