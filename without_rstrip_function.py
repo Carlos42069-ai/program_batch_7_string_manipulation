d901f565cd8fbc7b1b97628da65740214a2b110a
Carlos <carlosroque117@gmail.com>
2025-04-01 14:50:23 +0800

    Added a code that asks user for input then made a code that removes the spaces after the word.

--- without_rstrip_function.py
+++ without_rstrip_function.py
@@ -1,5 +1,6 @@
 #Ask user for input with spaces after the words.
-
+ask_user = input("Enter a word with extra spaces after: ")
 #Remove ther extra spaces after the word.
-
+while user_input and user_input[-1] == " ":
+    user_input = user_input[:-1]
 #Print the outcome.
