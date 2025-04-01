4e5b8d186a86a2b216764de8cdde3744117d437d
Carlos <carlosroque117@gmail.com>
2025-04-01 16:36:27 +0800

    Pseodocode for program 6.

---
+++ without_using_rjust_function.py
@@ -0,0 +1,7 @@
+#Add spaces at the beginning of the string to complete the specified length without using rjust().
+
+#Calculate how many spaces to add.
+
+#Add spaces to the beginning.
+
+#Print result.
