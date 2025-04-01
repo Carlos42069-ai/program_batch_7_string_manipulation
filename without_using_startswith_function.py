1fd401c8a7962807a30c388974f77330eff71fae
Carlos <carlosroque117@gmail.com>
2025-04-01 16:31:42 +0800

    Prints out the result if it is true or false.

--- without_using_startswith_function.py
+++ without_using_startswith_function.py
@@ -6,3 +6,4 @@ starts_with = input("Enter the word to be checked in your input: ")
 print_results = ask_user[:len(starts_with)] == starts_with
 
 #Print if it is true or false.
+print("The result is: ", print_results)
