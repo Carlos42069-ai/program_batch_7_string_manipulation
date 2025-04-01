70dec77539037591b4ea11b791c170e21e4bfb5c
Carlos <carlosroque117@gmail.com>
2025-04-01 16:38:49 +0800

    Asks user for an input and the amount of characters wanted in the string then made a code that calculate how many spaces to add.

--- without_using_rjust_function.py
+++ without_using_rjust_function.py
@@ -1,7 +1,8 @@
 #Add spaces at the beginning of the string to complete the specified length without using rjust().
-
+user_input = input("Enter a string: ")
+total_length = int(input("Enter the desired total length: "))
 #Calculate how many spaces to add.
-
+spaces_to_add = total_length - len(user_input)
 #Add spaces to the beginning.
 
 #Print result.
