2fe100aab669ccf15317f2380d1697c26914d0f1
Carlos <carlosroque117@gmail.com>
2025-04-01 16:00:33 +0800

    Fixed the name errors and added a code that prints out the results.

--- without_using_upper_function.py
+++ without_using_upper_function.py
@@ -6,5 +6,6 @@ for char in ask_user:
     if 'a' <= char <= 'z':
         uppercase += chr(ord(char) - 32)
     else:
-        uppercase_result += char
-#Print the result. 
+        uppercase += char
+#Print the result.
+print("The output now is: ", uppercase)
