b09de1e42fc127589492c12e446d6fe06da96eba
Carlos <carlosroque117@gmail.com>
2025-04-01 15:06:26 +0800

    Prints out the word without the suffix inputed to be removed.

--- withous_using_remove_suffix.py
+++ withous_using_remove_suffix.py
@@ -1,8 +1,9 @@
 #Ask user for input.
-ask_user = input("Enter a sentence: ")
+ask_user = input("Enter a word: ")
 suffix = input("Enter the suffix to remove: ")
 #Remove the suffix without using remove suffix.
 if ask_user.endswith(suffix):
-    user_input = ask_user[:-len(suffix)]
+    ask_user = ask_user[:-len(suffix)]
 
 #Prints out the output.
+print("Modified string:", repr(ask_user))
